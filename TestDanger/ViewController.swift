20c76b411279fe35eb0aa4c9797dcc4c6e4a5f07
Derek Bronston <dbronston@me.com>
2021-04-28 15:51:10 -0400

    udpated

---
+++ TestDanger/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestDanger
+//
+//  Created by Derek Bronston on 4/28/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
