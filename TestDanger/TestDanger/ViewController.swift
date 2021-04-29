ae07cec3911c0f88486432f1cf56ab2b13eab950
Derek Bronston <dbronston@me.com>
2021-04-29 14:32:44 -0400

    updated

--- TestDanger/TestDanger/ViewController.swift
+++ TestDanger/TestDanger/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let stringToSave = "The string I want to save"
+        let path = FileManager.default.urls(for: .applicationDirectory,
+                                            in: .userDomainMask)[0].appendingPathComponent("/.performance/myFile.json")
+
+        if let stringData = stringToSave.data(using: .utf8) {
+            try? stringData.write(to: path)
+        }
         // Do any additional setup after loading the view.
     }
 
